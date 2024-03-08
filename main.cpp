0b06ae25eff05fe434da492b599a86cfcfcbbff6
DawsonWhippleWSU <dlwhip@outlook.com>
2024-03-08 15:03:22 +0100

    added main function

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/****************************************************************************
+* Title:          SDL2
+* Author:         Dawson Whipple
+* Date:           3/7/24
+* Description:    Learning SDL2 library in c++
+****************************************************************************/
+#include <iostream>
+
+int main(){
+    return 0;
+}
