2159f24106e3745f6869255eef21703ebde4dd11
DawsonWhippleWSU <dlwhip@outlook.com>
2024-03-13 07:14:57 +0100

    Created basic snake game functionalities

--- project/main.cpp
+++ project/main.cpp
@@ -8,173 +8,134 @@
 *   - Able to accept user input, also draw rectangle (event.cpp)
 *   - Render a texture (texture.cpp)
 *   - Render an Image (image.cpp)
-*	- Rotate a 3d cube (cubeRotate.cpp)
+*	- Rotate a 3d cube (cubeRotate.cpp) (RotatingCube.exe)
 ****************************************************************************/
-#include "headers/screen.hpp"
-#include <numeric>
-#include <cmath>
+//for snake game screen.h is not required
+#include <SDL2/SDL.h>
+#include <vector>
+#include <algorithm>
+//snake body will be a doubly ended queue
+#include <deque>
 
-const float _x_ROTATE = 0.02;
-const float _y_ROTATE = 0.01;
-const float _z_ROTATE = 0.04;
-
-const int X_WINDOW_CENTER = WINDOW_WIDTH/2;
-const int Y_WINDOW_CENTER = WINDOW_HEIGHT/2;
-
-struct vec3{
-	float x, y, z;
-};
-
-struct connection{
-	int a, b;
-};
-
-void rotate(vec3& point, float x = 1, float y = 1, float z = 1){
-	//apply the rotation to the given point
-	//radian
-	float rad = 0;
-	//following is the function for rotatin points i guess (?)
-	//Don't quite remember geometry class
-	rad = x;
-	point.y = std::cos(rad) * point.y - std::sin(rad) * point.z;
-	point.z = std::sin(rad) * point.y + std::cos(rad) * point.z;
-
-	rad = y;
-	point.x = std::cos(rad) * point.x + std::sin(rad) * point.z;
-	point.z = -std::sin(rad) * point.x + std::cos(rad) * point.z;
-
-	rad = z;
-	point.x = std::cos(rad) * point.x - std::sin(rad) * point.y;
-	point.y = std::sin(rad) * point.x + std::cos(rad) * point.y;
-}
-
-//Function for drawing a line to the screen
-//x1, y1 are start point coords
-//x2, y2 are end point coords
-void line(Screen& screen, float x1, float y1, float x2, float y2){
-	//find difference between the two points
-	float dx = x2 - x1;
-	float dy = y2 - y1;
-
-	//find the length of the line
-	//pythagorean theorom
-	float length = std::sqrt((dx*dx) + (dy*dy));
-
-	//finds the angle of the line
-	//atan2 is a special function that prevents division by 0
-	float angle = std::atan2(dy, dx);
-
-	//use pixel function to draw the line
-	for(float i = 0; i < length; i++){
-		//getting the forward vector
-		//sin and cos are the vector math, * i scales up the pixels
-		screen.pixel(x1 + std::cos(angle) * i, 
-					 y1 + std::sin(angle) * i);
-	}
-}
+const int WINDOW_WIDTH = 1000/2;
+const int WINDOW_HEIGHT = 1000/2;
 
 //main function arguments are required for SDL2
 int main(int argc, char* argv[]){
-    Screen screen;
-	//draws 100 random pixels to the screen
-	// for (int i = 0; i < 100; i++){
-	// 	screen.pixel(rand()%640, rand()%480);
-	// }
+	SDL_Init(SDL_INIT_EVERYTHING);
+	auto window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+	auto renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Event e;
 
-	//draws a square on the screen
-	// line(screen, 100, 100, 200, 100);
-	// line(screen, 200, 100, 200, 200);
-	// line(screen, 200, 200, 100, 200);
-	// line(screen, 100, 200, 100, 100);
-
-	//all points in the cube
-	//the vertices
-	// std::vector<vec3> points{
-	// 	{X_WINDOW_CENTER + 100, Y_WINDOW_CENTER + 100, 100},
-	// 	{X_WINDOW_CENTER + 200, Y_WINDOW_CENTER + 100, 100},
-	// 	{X_WINDOW_CENTER + 200, Y_WINDOW_CENTER + 200, 100},
-	// 	{X_WINDOW_CENTER + 100, Y_WINDOW_CENTER + 200, 100},
-
-	// 	{X_WINDOW_CENTER + 100, Y_WINDOW_CENTER + 100, 200},
-	// 	{X_WINDOW_CENTER + 200, Y_WINDOW_CENTER + 100, 200},
-	// 	{X_WINDOW_CENTER + 200, Y_WINDOW_CENTER + 200, 200},
-	// 	{X_WINDOW_CENTER + 100, Y_WINDOW_CENTER + 200, 200},
-	// };
-	//Draws relative to center point of the screen
-	std::vector<vec3> points{
-		{X_WINDOW_CENTER - 50, Y_WINDOW_CENTER - 50, -50},
-		{X_WINDOW_CENTER + 50, Y_WINDOW_CENTER - 50, -50},
-		{X_WINDOW_CENTER + 50, Y_WINDOW_CENTER + 50, -50},
-		{X_WINDOW_CENTER - 50, Y_WINDOW_CENTER + 50, -50},
-
-		{X_WINDOW_CENTER - 50, Y_WINDOW_CENTER - 50, 50},
-		{X_WINDOW_CENTER + 50, Y_WINDOW_CENTER - 50, 50},
-		{X_WINDOW_CENTER + 50, Y_WINDOW_CENTER + 50, 50},
-		{X_WINDOW_CENTER - 50, Y_WINDOW_CENTER + 50, 50},
-	};
-	
-	//defines all connections for the vertices of the square
-	//the edges
-	std::vector<connection> connections{
-		{0,4},
-		{1,5},
-		{2,6},
-		{3,7},
-
-		{0,1},
-		{1,2},
-		{2,3},
-		{3,0},
-
-		{4,5},
-		{5,6},
-		{6,7},
-		{7,4}
+	enum Direction{
+		DOWN, LEFT, RIGHT, UP
 	};
 
-	//find the center point of the cube
-	vec3 centeroid{0, 0, 0};
-	for(auto& p : points){
-		centeroid.x += p.x;
-		centeroid.y += p.y;
-		centeroid.z += p.z;
+	bool running = true;
+	//direction the snake is going
+	int dir = 0;
+	//create the head of the snake at the center of the screen
+	SDL_Rect head {WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 10, 10};
+
+	//snake body container
+	std::deque<SDL_Rect> rq;
+	//size of snake
+	int size = 1;
+	//apple container
+	std::vector<SDL_Rect> apples;
+
+	//create apples on the map
+	for(int i = 0; i < 100; i++){
+		SDL_Rect rect = {rand()%100 *10, rand()%100 * 10, 10, 10};
+    	apples.push_back(rect);
 	}
-	//divide the total of all points by the number of vertices in the shape
-	//in this case there are 8 for a cube
-	//can also think of it as dividing the centroid by the size of the vector of vertice points
-	centeroid.x /= points.size();	
-	centeroid.y /= points.size();
-	centeroid.z /= points.size();
 
 	//game loop
-	while(true){
-		//rotate all points
-		//then draw pixels to the screen
-		//standard method of rotating in computer graphics
-		for(auto& p : points){
-			p.x -= centeroid.x;
-			p.y -= centeroid.y;
-			p.z -= centeroid.z;
-			rotate(p, 0.002, 0.001, 0.004);
-			p.x += centeroid.x;
-			p.y += centeroid.y;
-			p.z += centeroid.z;
-			screen.pixel(p.x, p.y);
+	while(running){
+		//Check user input
+		while(SDL_PollEvent(&e)){
+			if(e.type == SDL_QUIT){
+				running = false;
+			}
+			//check for user input
+			if(e.type == SDL_KEYDOWN){
+				//check which specific key was pressed
+				switch(e.key.keysym.sym){
+					case SDLK_DOWN:
+						dir = DOWN;
+						break;
+					case SDLK_UP:
+						dir = UP;
+						break;
+					case SDLK_LEFT:
+						dir = LEFT;
+						break;
+					case SDLK_RIGHT:
+						dir = RIGHT;
+						break;
+				}
+			}
 		}
-		for(auto& conn : connections){
-			line(screen, points[conn.a].x, 
-						 points[conn.a].y,
-						 points[conn.b].x,
-						 points[conn.b].y);
+		//move
+		switch(dir){
+			case DOWN:
+				head.y += 10;
+				break;
+			case UP:
+				head.y -= 10;
+				break;
+			case LEFT:
+				head.x -= 10;
+				break;
+			case RIGHT:
+				head.x += 10;
+				break;
 		}
-		screen.show();
-		screen.clear();
+		//collision detection with apple
+		std::for_each(apples.begin(), apples.end(), [&](auto& apple){
+			//check every single apple against the snake
+			if(head.x == apple.x && head.y == apple.y){
+				//increase size of snake
+				size += 10;
+				//move apple to off screen
+				apple.x = -10;
+				apple.y = -10;
+			}
+		});
+		//collision detection with snake body
+		std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment){
+			if(head.x == snake_segment.x && head.y == snake_segment.y){
+				size = 1;
+			}
+		});
+		//put head to the front of the queue
+		rq.push_front(head);
+		//pop tail from queue
+		while(rq.size() > size){
+			rq.pop_back();
+		}
 
-		screen.input();
-		//delays for 30 milliseconds
-		SDL_Delay(5);
+		//clear window
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		//Draw Body
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		//render the whole snake including head
+		std::for_each(rq.begin(), rq.end(), [&](auto& snake_segment){
+			//draw rect to the screen
+			SDL_RenderFillRect(renderer, &snake_segment);
+		});
+		//Draw apples
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		std::for_each(apples.begin(), apples.end(), [&](auto& apple){
+			SDL_RenderFillRect(renderer, &apple);
+		});
+
+		//display
+		SDL_RenderPresent(renderer);
+		SDL_Delay(25);
 	}
-
     return 0;
 }
 //all:
